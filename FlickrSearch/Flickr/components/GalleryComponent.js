50c9cc2ea95cc0f49c61ce857cb9fc8f7c700130
Bhupendra <bhupendrasharma18@gmail.com>
2020-05-16 13:13:06 +0530

    Dynamic item width and dynamic columns

--- FlickrSearch/Flickr/components/GalleryComponent.js
+++ FlickrSearch/Flickr/components/GalleryComponent.js
@@ -1,32 +1,39 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, FlatList, Image } from 'react-native';
+import { View, Text, StyleSheet, FlatList, Image, Dimensions } from 'react-native';
 
-function Item({ title, imgUrl }) {
+const { width, height } = Dimensions.get('window');
+const SCREEN_WIDTH = width
+const PRODUCT_ITEM_MARGIN = 5
+
+function Item({ title, imgUrl, itemWidth }) {
   return (
-    <View style={styles.item}>
+    <View style={[styles.item, {width: itemWidth}]}>
       {/* <Text style={styles.title}>{title}</Text> */}
       {imgUrl ? <Image style={styles.image} source={{uri: imgUrl}} /> : null}
     </View>
   );
 }
 
+const itemWidth = column => {
+  return (SCREEN_WIDTH / column) - (column-1) * PRODUCT_ITEM_MARGIN
+}
+
 class GalleryComponent extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      itemWidth: itemWidth(this.props.columns)
     };
   }
 
   render() {
-    console.log(this.props);
-    
     return (
       <View>
         <View style={styles.container}>
           <FlatList
             data={this.props.data}
-            numColumns={2}
-            renderItem={({ item }) => <Item title={item.title} imgUrl={item.url_s} />}
+            numColumns={this.props.columns}
+            renderItem={({ item }) => <Item title={item.title} imgUrl={item.url_s} itemWidth={this.state.itemWidth} />}
             keyExtractor={item => item.id}
           />
         </View>
@@ -41,15 +48,18 @@ const styles = StyleSheet.create({
   },
   item: {
     backgroundColor: '#f9c2ff',
-    padding: 20,
-    marginVertical: 8,
-    marginHorizontal: 16,
+    // padding: 20,
+    // marginVertical: 8,
+    // marginHorizontal: 16,
+    marginHorizontal: PRODUCT_ITEM_MARGIN,
+    borderWidth: 1,
+    borderColor: 'blue',
   },
   title: {
     fontSize: 32,
   },
   image: {
-    width: 100,
+    width: 70,
     height: 100,
     backgroundColor: 'red'
   }
--- FlickrSearch/Flickr/index.js
+++ FlickrSearch/Flickr/index.js
@@ -15,7 +15,7 @@ class Flickr extends Component {
       <View>
         <Text> index </Text>
         <Search></Search>
-        <Gallery data={DATA.photos.photo}></Gallery>
+        <Gallery data={DATA.photos.photo} columns={2}></Gallery>
       </View>
     );
   }
@@ -23,4 +23,4 @@ class Flickr extends Component {
 
 export default Flickr;
 
-const DATA = {"photos":{"page":1,"pages":92844,"perpage":2,"total":"185687","photo":[{"id":"49897610703","owner":"91751276@N08","secret":"5e04f05b5c","server":"65535","farm":66,"title":"wk 20...cat or kitten","ispublic":1,"isfriend":0,"isfamily":0,"url_s":"https:\/\/live.staticflickr.com\/65535\/49897610703_5e04f05b5c_m.jpg","height_s":240,"width_s":193},{"id":"49897485593","owner":"142178697@N05","secret":"ba3456f63c","server":"65535","farm":66,"title":"KuroMika 5034.jpg","ispublic":1,"isfriend":0,"isfamily":0,"url_s":"https:\/\/live.staticflickr.com\/65535\/49897485593_ba3456f63c_m.jpg","height_s":160,"width_s":240}]},"stat":"ok"}
+const DATA = {"photos":{"page":1,"pages":92844,"perpage":2,"total":"185687","photo":[{"id":"49897610703","owner":"91751276@N08","secret":"5e04f05b5c","server":"65535","farm":66,"title":"wk 20...cat or kitten","ispublic":1,"isfriend":0,"isfamily":0,"url_s":"https:\/\/live.staticflickr.com\/65535\/49897610703_5e04f05b5c_m.jpg","height_s":240,"width_s":193},{"id":"49897485593","owner":"142178697@N05","secret":"ba3456f63c","server":"65535","farm":66,"title":"KuroMika 5034.jpg","ispublic":1,"isfriend":0,"isfamily":0,"url_s":"https:\/\/live.staticflickr.com\/65535\/49897485593_ba3456f63c_m.jpg","height_s":160,"width_s":240},{"id":"49897485598","owner":"142178697@N05","secret":"ba3456f63c","server":"65535","farm":66,"title":"KuroMika 5034.jpg","ispublic":1,"isfriend":0,"isfamily":0,"url_s":"https:\/\/live.staticflickr.com\/65535\/49897485593_ba3456f63c_m.jpg","height_s":160,"width_s":240},{"id":"49897610709","owner":"91751276@N08","secret":"5e04f05b5c","server":"65535","farm":66,"title":"wk 20...cat or kitten","ispublic":1,"isfriend":0,"isfamily":0,"url_s":"https:\/\/live.staticflickr.com\/65535\/49897610703_5e04f05b5c_m.jpg","height_s":240,"width_s":193}]},"stat":"ok"}
